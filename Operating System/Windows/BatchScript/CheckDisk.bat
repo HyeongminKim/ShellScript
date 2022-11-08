5a51bc442adfbac1bede6883287f0882912e707c
HyeongminKim <assa0620@gmail.com>
2022-11-08 23:09:08 +0900

    OS 이미지 스캔방식 변경

--- Operating System/Windows/BatchScript/CheckDisk.bat
+++ Operating System/Windows/BatchScript/CheckDisk.bat
@@ -6,10 +6,10 @@ if NOT %ERRORLEVEL% == 0 (
     exit /B 1
 )
 
-:: Dism -Online -Cleanup-Image -CheckHealth
-:: Dism -Online -Cleanup-Image -ScanHealth
+Dism -Online -Cleanup-Image -CheckHealth
+::Dism -Online -Cleanup-Image -ScanHealth
 
-Dism -Online -Cleanup-Image -RestoreHealth
+::Dism -Online -Cleanup-Image -RestoreHealth
 sfc -scannow
 
 chkdsk C: /F
