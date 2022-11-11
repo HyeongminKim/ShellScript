b52f0726f41be6f1b0c64f67501b2c34380e43b6
HyeongminKim <assa0620@gmail.com>
2022-11-11 16:26:38 +0900

    버그 수정

--- Operating System/Windows/Python/DiabloLauncher.py
+++ Operating System/Windows/Python/DiabloLauncher.py
@@ -788,11 +788,11 @@ def init():
         os.system('explorer https://github.com/HyeongminKim/ShellScript')
 
     menubar = Menu(root)
-    toolsMenu = Menu(menubar, tearoff=0)
+    toolsMenu = Menu(menubar)
     toolsMenu.add_command(label='통계 재설정...', command=ResetGameStatus)
     menubar.add_cascade(label='도구', menu=toolsMenu)
 
-    helpMenu = Menu(menubar, tearoff=0)
+    helpMenu = Menu(menubar)
     helpMenu.add_command(label='GitHub 방문', command=OpenDevSite)
     menubar.add_cascade(label='도움말', menu=helpMenu)
 
@@ -843,6 +843,8 @@ def init():
     statusbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
 
     ReloadStatusBar()
+
+    root.config(menu=menubar)
 
     root.mainloop()
 
