9ec934dab010fedcbf2240cd860f6978bd7973ec
HyeongminKim <assa0620@gmail.com>
2024-11-15 18:25:07 +0900

    전체 게시물수 추가 및 스타일 변경

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11152
+// @version     2024.11153
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -175,7 +175,7 @@ contentsCount.forEach(element => {
 
     if(hiddenContentsCount.length > 0) {
       boldElement.innerText = number + " (-" + hiddenContentsCount.length + ")";
-      boldElement.title = boldElement.title + " / 필터링됨: " + hiddenContentsCount.length;
+      boldElement.title = boldElement.title + " | 필터링됨: " + hiddenContentsCount.length + " / 30";
     }
 
     if(hiddenContentsCount.length > 25) {
