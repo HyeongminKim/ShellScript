661087e42e200c5c58b971c3860de822ee97b4c7
HyeongminKim <assa0620@gmail.com>
2024-11-18 18:18:28 +0900

    스타일 갱신

--- Universal/Userscripts/다모앙/다모앙 검색 필드.js
+++ Universal/Userscripts/다모앙/다모앙 검색 필드.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11141
+// @version     2024.11180
 // @author      Hyeongmin Kim
 // @description 9/19/2024, 8:59:55 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%80%EC%83%89%20%ED%95%84%EB%93%9C.js
@@ -19,6 +19,9 @@ function addFilterInput() {
   input.inputMode = 'search';
   input.id = 'filter-input';
   input.placeholder = '게시판/카테고리 검색...';
+  input.style.margin = '10px 5px';
+  input.style.padding = '4px';
+  input.style.width = '200px';
   input.onkeyup = filterNavLinks;
 
   sidebar.insertBefore(input, sidebar.firstChild);
