bfe84abfd7f96984f48975d454c58dd0a8cb38c1
HyeongminKim <assa0620@gmail.com>
2024-11-18 17:55:24 +0900

    댓글 텍스트와 구분되도록 수정

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11180
+// @version     2024.11181
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -69,7 +69,7 @@ reportedcommentBlocks.forEach(element => {
   const rawData = element.textContent.trim().replace('🚨신고 누적 ', '');
 
   if(boldTag && boldTag.textContent.includes("🚨신고 누적")) {
-    element.textContent = '🚨신고 누적된 댓글입니다. 내용을 보시려면 여기를 클릭하세요.';
+    element.textContent = '[🚨신고 누적된 댓글입니다. 내용을 보시려면 여기를 클릭하세요.]';
 
     element.addEventListener('click', (event) => {
       event.preventDefault();
