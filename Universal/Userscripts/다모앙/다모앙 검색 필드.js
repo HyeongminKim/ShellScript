fb90533bab5738bacc98a7a8a9c544b81df8779d
HyeongminKim <assa0620@gmail.com>
2024-11-21 11:11:22 +0900

    텍스트박스 라운딩 처리

--- Universal/Userscripts/다모앙/다모앙 검색 필드.js
+++ Universal/Userscripts/다모앙/다모앙 검색 필드.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11180
+// @version     2024.11210
 // @author      Hyeongmin Kim
 // @description 9/19/2024, 8:59:55 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%80%EC%83%89%20%ED%95%84%EB%93%9C.js
@@ -22,6 +22,7 @@ function addFilterInput() {
   input.style.margin = '10px 5px';
   input.style.padding = '4px';
   input.style.width = '200px';
+  input.style.borderRadius = "10px";
   input.onkeyup = filterNavLinks;
 
   sidebar.insertBefore(input, sidebar.firstChild);
