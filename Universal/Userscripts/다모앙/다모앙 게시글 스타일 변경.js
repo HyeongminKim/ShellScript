b89cc4d118d66d4cebc9161fe7ac4bcf97e4e660
HyeongminKim <assa0620@gmail.com>
2024-11-21 22:22:03 +0900

    비로그인 상태일 경우 이를 명시하도록 업데이트

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11184
+// @version     2024.11210
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -40,6 +40,16 @@ schWordElements.forEach(element => {
 hotElements.forEach(element => {
     element.style.display = 'none';
 });
+
+if(!userOnline) {
+  const menubar = document.querySelector('.ms-auto');
+  if(menubar) {
+    const not_logind_text = document.createElement('b');
+    not_logind_text.style.color = 'magenta';
+    not_logind_text.innerHTML = '비로그인 상태 [RO]: 다른 회원들과 상호작용을 하고 다모앙 유저스크립트의 모든 기능을 활성화 하려면 먼저 로그인하세요.';
+    menubar.appendChild(not_logind_text);
+  }
+}
 
 linkBlocks.forEach(element => {
   if(!element.classList.contains('list-group-item') && !element.classList.contains('fw-normal')) {
