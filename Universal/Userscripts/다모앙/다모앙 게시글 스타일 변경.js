caa57c22cac4659c8663686277675b8e183066a3
HyeongminKim <assa0620@gmail.com>
2024-11-25 21:21:21 +0900

    비로그인시 메뉴바 버튼을 제거하도록 변경

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.11220
+// @version     2024.11250
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -43,7 +43,10 @@ hotElements.forEach(element => {
 
 if(!userOnline) {
   const menubar = document.querySelector('.ms-auto');
+  const targetMenu = document.querySelector('.site-nav d-none d-lg-block');
   const not_logind_text = document.createElement('b');
+
+  if(targetMenu) targetMenu.remove();
   not_logind_text.style.color = 'magenta';
   if(menubar && !(navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('Android') > -1)) {
     not_logind_text.innerHTML = '비로그인 상태 [RO]: ' + navigator.userAgent;
