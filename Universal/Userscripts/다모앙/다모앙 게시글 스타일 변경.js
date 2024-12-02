f84e6356b84b8c9cd087cd16d8778c36d7409165
HyeongminKim <assa0620@gmail.com>
2024-12-02 18:44:48 +0900

    마이너 버그 수정

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.12020
+// @version     2024.12021
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -26,7 +26,6 @@ const linkBlocks = document.querySelectorAll('.da-link-block');
 const reportedlinkBlocks = document.querySelectorAll('.da-link-block.subject-ellipsis');
 const reportedcommentBlocks = document.querySelectorAll('.na-convert');
 const hotElements = document.querySelectorAll('.na-icon');
-const recommendBtnStyle = document.createElement('style');
 const toTop = document.getElementById('toTop');
 
 const userOnline = (navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('Android') > -1) ? document.querySelector('.d-flex.justify-content-between.mb-1.small') : document.querySelector('.d-flex.align-items-center.justify-content-between.small');
@@ -258,11 +257,3 @@ if (searchElement) searchElement.className = '';
 if (searchUserName) searchUserName.textContent = '작성자 (이름)';
 if (searchUserID) searchUserID.textContent = '작성자 (ID)';
 
-recommendBtnStyle.innerHTML = `
-  .bi.bi-hand-thumbs-up::before {
-    content: '♥';
-  }
-`;
-
-document.head.appendChild(commentCountStyle);
-document.head.appendChild(recommendBtnStyle);
