f0f19a92797e8384433c6bdecf192dabaee31e04
HyeongminKim <assa0620@gmail.com>
2024-12-13 19:03:15 +0900

    `na-new`와 `na-hot`만 차단되도록 수정

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2024.12132
+// @version     2024.12133
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -25,7 +25,8 @@ const memberLeaveBtn = document.querySelectorAll('.bi.bi-box-arrow-right.fs-3');
 const linkBlocks = document.querySelectorAll('.da-link-block');
 const reportedlinkBlocks = document.querySelectorAll('.da-link-block.subject-ellipsis');
 const reportedcommentBlocks = document.querySelectorAll('.na-convert');
-const hotElements = document.querySelectorAll('.na-icon');
+const newElements = document.querySelectorAll('.na-icon.na-new');
+const hotElements = document.querySelectorAll('.na-icon.na-hot');
 const toTop = document.getElementById('toTop');
 const disciplines = document.querySelectorAll('.wr-period.text-nowrap.order-5.order-md-2');
 
@@ -36,6 +37,10 @@ if(toTop) toTop.remove();
 schWordElements.forEach(element => {
   element.style.backgroundColor = 'yellow';
   element.style.color = 'black';
+});
+
+newElements.forEach(element => {
+    element.style.display = 'none';
 });
 
 hotElements.forEach(element => {
