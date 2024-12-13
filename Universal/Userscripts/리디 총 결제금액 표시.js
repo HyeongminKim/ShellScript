c19950d141d083d9c1b278064ec7d40bd975ec05
HyeongminKim <assa0620@gmail.com>
2024-12-13 19:35:26 +0900

    업데이트 기능 추가

--- Universal/Userscripts/리디 총 결제금액 표시.js
+++ Universal/Userscripts/리디 총 결제금액 표시.js
@@ -3,9 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://ridibooks.com/order/history
 // @grant       none
-// @version     1.0
+// @version     2024.12130
 // @author      Hyeongmin Kim
 // @description 12/13/2024, 7:26:20 PM
+// @updateURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%A6%AC%EB%94%94%20%EC%B4%9D%20%EA%B2%B0%EC%A0%9C%EA%B8%88%EC%95%A1%20%ED%91%9C%EC%8B%9C.js
+// @downloadURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%A6%AC%EB%94%94%20%EC%B4%9D%20%EA%B2%B0%EC%A0%9C%EA%B8%88%EC%95%A1%20%ED%91%9C%EC%8B%9C.js
 // ==/UserScript==
 
 var total = 0;
