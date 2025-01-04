986607c3a95b2eb4f922b2260c0b8b5fcbf2abcc
Hyeongmin Kim <assa0620@gmail.com>
2025-01-04 22:41:13 +0900

    웹툰일 경우 강조 표시 추가

--- Universal/Userscripts/리디 스타일 변경.js
+++ Universal/Userscripts/리디 스타일 변경.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        리디 총 결제금액 표시
+// @name        리디 스타일 변경
 // @namespace   Violentmonkey Scripts
 // @match       https://ridibooks.com/order/history
 // @grant       none
-// @version     2024.12140
+// @version     2025.01040
 // @author      Hyeongmin Kim
 // @description 12/13/2024, 7:26:20 PM
 // @updateURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%A6%AC%EB%94%94%20%EC%B4%9D%20%EA%B2%B0%EC%A0%9C%EA%B8%88%EC%95%A1%20%ED%91%9C%EC%8B%9C.js
@@ -12,6 +12,7 @@
 
 var total = 0;
 const museoElements = document.querySelectorAll('.main_value');
+const webtoonElements = document.querySelectorAll('.b-kw94g7');
 
 museoElements.forEach(element => {
   const text = element.textContent.trim().replace(/,/g, '');
@@ -25,4 +26,10 @@ const pageTitleElement = document.querySelector('.pagetitle_text');
 if (pageTitleElement) {
   pageTitleElement.textContent += ` (총계: ${total.toLocaleString()}원)`;
 }
+
+webtoonElements.forEach(element => {
+  if(element.textContent.includes('화')) {
+    element.style.color = 'red';
+  }
+});
 
