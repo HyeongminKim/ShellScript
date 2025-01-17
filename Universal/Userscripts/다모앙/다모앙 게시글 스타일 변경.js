7796ff4c95981170bdb287021cbf7ce9abc58d85
Hyeongmin Kim <assa0620@gmail.com>
2025-01-17 22:57:08 +0900

    공감수 스타일 제거

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2025.01060
+// @version     2025.01170
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -154,7 +154,6 @@ recommendElements.forEach(element => {
         element.textContent = formattedNumber;
         element.title = '받은 추천수: ' + number.toLocaleString(navigator.language);
       }
-      element.textContent = '♥ ' + element.textContent;
     }
   })
 });
