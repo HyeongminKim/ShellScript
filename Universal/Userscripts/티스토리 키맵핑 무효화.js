ebe11b3e4410f4e0959ab74a9480c4f79cca6deb
Hyeongmin Kim <assa0620@gmail.com>
2025-02-01 23:28:44 +0900

    키 이벤트를 차단하지 못하던 문제 수정

--- Universal/Userscripts/티스토리 키맵핑 무효화.js
+++ Universal/Userscripts/티스토리 키맵핑 무효화.js
@@ -3,26 +3,25 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*.tistory.com/*
 // @grant       none
-// @version     2025.02010
+// @version     2025.02011
 // @author      Hyeongmin Kim
 // @description 2/1/2025, 10:47:43 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%ED%8B%B0%EC%8A%A4%ED%86%A0%EB%A6%AC%20%ED%82%A4%EB%A7%B5%ED%95%91%20%EB%AC%B4%ED%9A%A8%ED%99%94.js
 // @downloadURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%ED%8B%B0%EC%8A%A4%ED%86%A0%EB%A6%AC%20%ED%82%A4%EB%A7%B5%ED%95%91%20%EB%AC%B4%ED%9A%A8%ED%99%94.js
 // ==/UserScript==
 
-window.onload = function() {
-  setTimeout(function() {
-    window.onkeydown = function(event) {
-      event.preventDefault();
-    };
+document.addEventListener("keydown", (event) => {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+}, true);
 
-    window.onkeypress = function(event) {
-      event.preventDefault();
-    };
+document.addEventListener("keyup", (event) => {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+}, true);
 
-    window.onkeyup = function(event) {
-      event.preventDefault();
-    };
-  }, 1000);
-};
+document.addEventListener("keypress", (event) => {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+}, true);
 
