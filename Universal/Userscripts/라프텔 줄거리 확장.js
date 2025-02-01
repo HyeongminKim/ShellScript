c5a02e3c81766f7b9c481f9e69092a2fadd766f9
Hyeongmin Kim <assa0620@gmail.com>
2025-02-01 23:10:10 +0900

    업데이트 URL 추가

--- Universal/Userscripts/라프텔 줄거리 확장.js
+++ Universal/Userscripts/라프텔 줄거리 확장.js
@@ -3,9 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://laftel.net/*
 // @grant       none
-// @version     1.0
+// @version     2025.02010
 // @author      Hyeongmin Kim
 // @description 2/1/2025, 10:55:01 PM
+// @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%9D%BC%ED%94%84%ED%85%94%20%EC%A4%84%EA%B1%B0%EB%A6%AC%20%ED%99%95%EC%9E%A5.js
+// @downloadURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%9D%BC%ED%94%84%ED%85%94%20%EC%A4%84%EA%B1%B0%EB%A6%AC%20%ED%99%95%EC%9E%A5.js
 // ==/UserScript==
 
 const observer = new MutationObserver(() => {
