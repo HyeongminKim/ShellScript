066a1378bfb89c3a1f9d969f142c6658abf9466f
Hyeongmin Kim <assa0620@gmail.com>
2025-02-16 13:41:50 +0900

    캘린더 title 태그 추가

--- Universal/Userscripts/리디 스타일 변경.js
+++ Universal/Userscripts/리디 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://ridibooks.com/*
 // @grant       none
-// @version     2025.02090
+// @version     2025.02160
 // @author      Hyeongmin Kim
 // @description 12/13/2024, 7:26:20 PM
 // @updateURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%A6%AC%EB%94%94%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -18,6 +18,7 @@ const webtoonElements = document.querySelectorAll('.b-kw94g7');
 const pageTitleElement = document.querySelector('.pagetitle_text');
 const pageBtnElements = document.querySelectorAll('.page_list');
 const recommendElements = document.querySelector('.rigrid-a6cwv8');
+const imgElements = document.querySelectorAll(".EventCalendar_TableBadge");
 
 pageBtnElements.forEach(element => {
   const pageThis = element.classList.contains("page_this");
@@ -51,5 +52,21 @@ webtoonElements.forEach(element => {
 
 document.querySelectorAll(".rigrid-11zvsio").forEach(element => {
   element.title = element.textContent.trim();
+});
+
+imgElements.forEach(element => {
+  const src = element.getAttribute("src");
+  var titleText = "";
+
+  if (src.includes("482261")) {
+    titleText = "후속권 업데이트";
+  } else if(src.includes("434923")) {
+    titleText = "세트전";
+  } else if(src.includes("529079")) {
+    titleText = "RIDI ONLY";
+  } else if(src.includes("031013")) {
+    titleText = "신작";
+  }
+  element.setAttribute("title", titleText);
 });
 
