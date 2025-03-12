2e2f1fc859a460fa0d0026b775640c37a8906b73
HyeongminKim <assa0620@gmail.com>
2025-03-12 15:01:13 +0900

    유튜브 라이브챗 타이틀 스크립트 추가

---
+++ Universal/Userscripts/유튜브 라이브챗 타이틀 생성.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        유튜브 라이브챗 타이틀 생성
+// @namespace   Violentmonkey Scripts
+// @match       *://www.youtube.com/live_chat*
+// @grant       none
+// @version     2025.03120
+// @author      Hyeongmin Kim
+// @description 3/12/2025, 2:53:53 PM
+// ==/UserScript==
+
+document.title = "Youtube - 실시간 채팅";
+
