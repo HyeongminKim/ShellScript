681967fd4daecafe32fde764904fcd2efd01b89f
HyeongminKim <assa0620@gmail.com>
2025-03-12 15:02:58 +0900

    업데이트 주소 추가

--- Universal/Userscripts/유튜브 라이브챗 타이틀 생성.js
+++ Universal/Userscripts/유튜브 라이브챗 타이틀 생성.js
@@ -3,9 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://www.youtube.com/live_chat*
 // @grant       none
-// @version     2025.03120
+// @version     2025.03121
 // @author      Hyeongmin Kim
 // @description 3/12/2025, 2:53:53 PM
+// @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EC%9C%A0%ED%8A%9C%EB%B8%8C%20%EB%9D%BC%EC%9D%B4%EB%B8%8C%EC%B1%97%20%ED%83%80%EC%9D%B4%ED%8B%80%20%EC%83%9D%EC%84%B1.js
+// @downloadURL https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EC%9C%A0%ED%8A%9C%EB%B8%8C%20%EB%9D%BC%EC%9D%B4%EB%B8%8C%EC%B1%97%20%ED%83%80%EC%9D%B4%ED%8B%80%20%EC%83%9D%EC%84%B1.js
 // ==/UserScript==
 
 document.title = "Youtube - 실시간 채팅";
