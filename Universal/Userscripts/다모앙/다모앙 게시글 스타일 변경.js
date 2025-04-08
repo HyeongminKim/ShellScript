b83bd005d0b9654de2d5006184ec5917f28bd90d
Hyeongmin Kim <assa0620@gmail.com>
2025-04-08 19:30:29 +0900

    하단 검색창 옵션 텍스트가 적용되지 않은 문제 수정

--- Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
+++ Universal/Userscripts/다모앙/다모앙 게시글 스타일 변경.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://damoang.net/*
 // @grant       none
-// @version     2025.02132
+// @version     2025.04080
 // @author      Hyeongmin Kim
 // @description 9/13/2024, 3:13:33 PM
 // @updateURL   https://raw.githubusercontent.com/HyeongminKim/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EA%B2%8C%EC%8B%9C%EA%B8%80%20%EC%8A%A4%ED%83%80%EC%9D%BC%20%EB%B3%80%EA%B2%BD.js
@@ -244,10 +244,13 @@ document.querySelectorAll('[id^="c_"]').forEach(element => {
 });
 
 const searchElement = document.getElementById('boardSearch');
-const searchUserName = document.getElementById('bo_sfl').querySelector('option[value="wr_name,1"]');
-const searchUserID = document.getElementById('bo_sfl').querySelector('option[value="wr_name,0"]');
-
 if (searchElement) searchElement.className = '';
-if (searchUserName) searchUserName.textContent = '작성자 (이름)';
-if (searchUserID) searchUserID.textContent = '작성자 (ID)';
+
+document.querySelectorAll('select#bo_sfl option').forEach(option => {
+  if (option.value === 'wr_name,1') {
+    option.textContent = '작성자(이름)';
+  } else if (option.value === 'wr_name,0') {
+    option.textContent = '작성자(ID)';
+  }
+});
 
