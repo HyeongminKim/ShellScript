77b2c76a9f5266fc9a1cd3877b32e57db4b3f6f6
HyeongminKim <assa0620@gmail.com>
2025-06-08 01:28:24 +0900

    discord-bot 기능 구현

--- Universal/OBS Studio/discord-bot.py
+++ Universal/OBS Studio/discord-bot.py
@@ -1,42 +1,48 @@
-import os
+import os, sys, subprocess
+
 import discord
-import subprocess
+from discord import app_commands
 
-intents = discord.Intents.default()
-bot = discord.Bot(intents=intents)
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = discord.Intents.all())
+        self.synced = False
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
 
-@bot.event
-async def on_ready():
-    print(f"✅ 로그인 성공: {bot.user}")
+client = aclient()
+tree = app_commands.CommandTree(client)
 
-@bot.slash_command(name="status", description="현재 재생 중인 음악 정보를 출력합니다.")
-async def status(ctx: discord.ApplicationContext):
-    await ctx.defer()  # 봇이 응답 중이라는 표시
 
+# 슬래시 옵션 리스트
+@tree.command(name="status", description="현재 재생 중인 음악 정보를 출력합니다.")
+async def status(interaction: discord.Interaction):
     try:
         # playerctl metadata 실행
         result = subprocess.check_output(
-            ["playerctl", "metadata", "--format",
-                "제목: {{title}}\n",
-                "URL: {{xesam:url}}\n",
-                "{{duration(position)}} | {{duration(mpris:length)}}\n"
-            ],
+            ["playerctl", "metadata", "--format", "제목: {{trunc(title, 27)}}\nURL: {{trunc(xesam:url, 27)}}\n{{duration(position)}} | {{duration(mpris:length)}}"],
             text=True
         )
         if not result:
             result = "🎵 현재 재생 중인 트랙이 없습니다."
         else:
-            await ctx.respond(f"🎵 현재 재생 정보:\n```\n{result}\n```")
+            await interaction.response.send_message(f"### 🎵 현재 재생 정보:\n```\n{result}\n```", ephemeral=True)
     except subprocess.CalledProcessError:
-        await ctx.respond("⚠️ playerctl 실행에 실패했습니다.")
+        await interaction.response.send_message("⚠️ playerctl 실행에 실패했습니다.", ephemeral=True)
+        sys.exit(1)
     except FileNotFoundError:
-        await ctx.respond("❌ 시스템에 playerctl이 설치되어 있지 않습니다.")
+        await interaction.response.send_message("❌ 시스템에 playerctl이 설치되어 있지 않습니다.", ephemeral=True)
+        sys.exit(1)
+
 
 # 봇 실행
 token = os.environ.get("DISCORD_BOT_PLAYERCTL")
-
 if token:
-    bot.run(token)
+    client.run(token)
 else:
-    print("❌ 환경변수 'OBS_MEDIAINFO_DISCORD_BOT'이 설정되지 않았습니다.")
+    print("❌ 환경변수 'DISCORD_BOT_PLAYERCTL'이 설정되지 않았습니다. 비공개 토큰을 설정해 주세요.")
+    sys.exit(1)
 
