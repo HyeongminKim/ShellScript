7b4ba875cb2da65be13a1f6469560cdeee3134c2
HyeongminKim <assa0620@gmail.com>
2025-06-09 14:45:18 +0900

    페이지 스타일 변경

--- Universal/Userscripts/스마트민방위교육 UX 최적화.js
+++ Universal/Userscripts/스마트민방위교육 UX 최적화.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://www.cdec.kr/*
 // @grant       none
-// @version     2024.11141
+// @version     2025.06090
 // @author      Hyeongmin Kim
 // @icon        https://i.namu.wiki/i/2R9UASCEkjwtVVvcP4q0r765eu3xJ_a5e8o6y7C2c_OgWtzJTX9Z_2pdxf3Wo11HPjgXi3Lg_71TlkotyM7DSw.svg
 // @description 9/9/2024, 민방위 학습 플레이어 크기 및 볼륨값 재지정
@@ -21,4 +21,9 @@ const volumeLevels = document.querySelectorAll('.vjs-volume-level');
 volumeLevels.forEach(element => {
   element.style.width = '25%';
 });
+
+const educationContents = document.getElementById('educationContents');
+if (educationContents) {
+  educationContents.style.margin = '0'; // margin 비활성화
+}
 
